fd41587068920b5a41aa437949e43bd1c6d5892f
Kenneth Mead <kennethmead784@gmail.com>
2024-09-18 13:59:23 -0400

    Updated file structure

---
+++ environments/README.md
@@ -0,0 +1,1 @@
+Copy the files inside the folder of the desired environment and paste them at the root of the `.devcontainer` folder for VS Code to detect the files when building dev containers
--- scripts/setup-ssh-env.sh
+++ scripts/setup-ssh-env.sh
@@ -1,3 +1,5 @@
+source .devcontainer/copy/colors.sh
+
 if [[ -z $1 ]]; then
   SSH_KEYNAME="github"
 else
@@ -9,27 +11,21 @@ else
   SSH_HOSTNAME=$2
 fi
 
-echo "Setting up Git env variables"
+echo "Setting up environment variables for Git and SSH"
 GIT_USER="$(git config --global user.name)"
 GIT_EMAIL="$(git config --global user.email)"
 
-echo "Setting up SSH env variables"
 # update ssh key filename as necessary
 SSH_PUBLIC="$(cat ~/.ssh/$SSH_KEYNAME.pub)"
 # replace all new line characters with '\n'
 # see .bashrc for how its converted back to new lines when inside the container
 SSH_PRIVATE="$(sed ':a;N;$!ba;s/\n/\\n/g' ~/.ssh/$SSH_KEYNAME)"
-echo "$SSH_PRIVATE"
-echo
 NL=$'\n'
-envString="GIT_USER=\"$GIT_USER\"${NL}GIT_EMAIL=$GIT_EMAIL${NL}SSH_KEYNAME=\"$SSH_KEYNAME\"${NL}SSH_HOSTNAME=\"$SSH_HOSTNAME\"${NL}SSH_PUBLIC=\"$SSH_PUBLIC\"${NL}SSH_PRIVATE=\"$SSH_PRIVATE\""
-echo "$envString"
-echo
+envString="GIT_USER=$GIT_USER${NL}GIT_EMAIL=$GIT_EMAIL${NL}SSH_KEYNAME=$SSH_KEYNAME${NL}SSH_HOSTNAME=$SSH_HOSTNAME${NL}SSH_PUBLIC=$SSH_PUBLIC${NL}SSH_PRIVATE=$SSH_PRIVATE"
 envPath=".devcontainer/.env"
-echo "Writing variables to '$envPath'"
-echo
-echo -e "$envString"
-echo
+echo -e "Writing variables to ${COLOR_LIGHT_PURPLE}$envPath${COLOR_NC}"
+echo -e "${COLOR_LIGHT_GREEN}Wrote variables to ${COLOR_LIGHT_PURPLE}$envPath${COLOR_NC}"
+echo -e "${COLOR_LIGHT_CYAN}$envString${COLOR_NC}"
 rm -f $envPath
 touch $envPath
 echo "$envString" > "$envPath"
