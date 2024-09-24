88168778c02c49e598261a027da7a6bd5a0936d6
Kenneth Mead <kennethmead784@gmail.com>
2024-09-24 11:40:28 -0400

    Updated readmes and added better windows support

--- environments/README.md
+++ environments/README.md
@@ -1,1 +1,3 @@
 Copy the files inside the folder of the desired environment and paste them at the root of the `.devcontainer` folder for VS Code to detect the files when building dev containers
+
+It's intended for VS Code to interact only with the dockerfiles in each environment folder. The docker compose files may use relative paths that would cause them to break if moved
--- scripts/wsl-setup-ssh.sh
+++ scripts/wsl-setup-ssh.sh
@@ -10,10 +10,20 @@ WINDOWS_USER=$WSL_USER
 CONTAINER_USER="root"
 USAGE="usage: wsl-setup-ssh.sh [--windows-user STRING] [--wsl-user STRING] [--container-user STRING]
 
-WARNING: runs sudo when setting ssh file ownership
+WARNING: runs sudo when setting file permissions
+
+For more, see https://serverfault.com/a/253314
+
+Path	              Permission
+.ssh directories    0700 (drwx------)
+private keys        0600 (-rw-------)
+config              0600 (-rw-------)
+public keys         0644 (-rw-r--r--)
+authorized_keys     0644 (-rw-r--r--)
+known_hosts         0644 (-rw-r--r--) 
 
 Copies the windows /c/users/$WINDOWS_USER/.ssh folder to wsl at /home/$WSL_USER/.ssh and sets the chown of each file to '$CONTAINER_USER'. 
-File permissions probably need to be checked manually
+File permissions might need to be checked manually
 
 Assumptions
 - SSH is already setup properly in windows
@@ -65,8 +75,10 @@ WSL_SSH_FOLDER=/home/$WSL_USER/.ssh
 echo "copying ssh folder from $WINDOWS_SSH_FOLDER to $WSL_SSH_FOLDER, setting read-write permissions on keys, and changing ownership to $CONTAINER_USER"
 mkdir $WSL_SSH_FOLDER
 cp -rf $WINDOWS_SSH_FOLDER/* $WSL_SSH_FOLDER
-sudo chown -R -c $CONTAINER_USER $WSL_SSH_FOLDER
-sudo chmod -R -c 0600 $WSL_SSH_FOLDER
-sudo find $WSL_SSH_FOLDER -name known_hosts -type f -exec chmod 644 {} \;
-sudo find $WSL_SSH_FOLDER -name "*.pub"     -type f -exec chmod 644 {} \;
-sudo chmod 0700 $WSL_SSH_FOLDER
+# bad owner or permissions on ~/.ssh/config
+# https://serverfault.com/questions/253313/ssh-returns-bad-owner-or-permissions-on-ssh-config#:~:text=These%20commands%20should%20fix%20the%20permission%20problem%3A%20chown,more%20files%20are%20affected%2C%20replace%20config%20with%20%2A.
+sudo chown -R -v $CONTAINER_USER $WSL_SSH_FOLDER
+sudo chmod -R -v 0600 $WSL_SSH_FOLDER
+sudo find $WSL_SSH_FOLDER -name known_hosts -type f -exec chmod -v 644 {} \;
+sudo find $WSL_SSH_FOLDER -name "*.pub"     -type f -exec chmod -v 644 {} \;
+sudo chmod -v 0700 $WSL_SSH_FOLDER
