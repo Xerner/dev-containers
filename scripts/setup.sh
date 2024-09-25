1d3054565a7313216528ec75769d7d8f796f8065
Kenneth <kennethmead784@gmail.com>
2024-09-25 18:12:19 +0000

    Added registry url as environment variable

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,12 +16,12 @@ GIT_EMAIL="$(git config --global user.email)"
 # .env contents
 envString="GIT_USER=$GIT_USER${NL}GIT_EMAIL=$GIT_EMAIL${NL}"
 # writing env file to temp directory
-echo -e "Writing variables to ${COLOR_LIGHT_PURPLE}$envPath${COLOR_NC}"
+echo -e "Writing variables to ${COLOR_PURPLE}$envPath${COLOR_NC}"
 envPath=".devcontainer/temp/.env"
 rm -f $envPath
 touch $envPath
 echo "$envString" > "$envPath"
-echo -e "${COLOR_LIGHT_GREEN}Wrote variables to ${COLOR_LIGHT_PURPLE}$envPath${COLOR_NC}"
+echo -e "${COLOR_GREEN}Wrote variables to ${COLOR_PURPLE}$envPath${COLOR_NC}"
 echo -e "${COLOR_LIGHT_CYAN}$envString${COLOR_NC}"
 
 #endregion
